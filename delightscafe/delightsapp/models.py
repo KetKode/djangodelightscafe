6fe99970b37de027bfef0d12cd0f186948038aa8
Anastasia <ketkoder@gmail.com>
2023-08-15 21:26:50 +0600

    updated the owner side

---
+++ delightscafe/delightsapp/models.py
@@ -0,0 +1,64 @@
+import datetime
+
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+# Ingredients, MenuItem,
+# RecipeRequirement, Purchase
+
+
+class Ingredient(models.Model):
+    """ Represents a single ingredient in the restaurant's inventory """
+    name = models.CharField(max_length=100, unique=True)
+    quantity = models.FloatField(default=0)
+    price_per_unit = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return "/ingredients"
+
+
+class RecipeRequirement (models.Model):
+    """ Represents a single ingredient required to make a meal on the restaurant's menu """
+    menu_item = models.ForeignKey (MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey (Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField (default=0)
+
+    def is_enough(self):
+        return self.quantity <= self.ingredient.quantity
+
+    def __str__(self):
+        return f"{self.menu_item}: {self.ingredient}"
+
+    def get_absolute_url(self):
+        return "/menu"
+
+
+class MenuItem(models.Model):
+    """ Represents a single entry on the restaurant's menu """
+    name = models.CharField(max_length=100, unique=True)
+    price_per_entry = models.FloatField(default=0)
+
+    def __str__(self):
+        return f"{self.name} / {self.price_per_entry}"
+
+    def available(self):
+        return all(X.enough() for X in self.reciperequirement_set.all())
+
+    def get_absolute_url(self):
+        return "/menu"
+
+
+class Purchase(models.Model):
+    """ Represents a single purchase of a meal on the restaurant's menu """
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    date_time_purchased = models.DateTimeField(default=datetime.date.today)
+
+    def __str__(self):
+        return f"{self.menu_item} / {self.date_time_purchased}"
+
+    def get_absolute_url(self):
+        return "/purchases"
